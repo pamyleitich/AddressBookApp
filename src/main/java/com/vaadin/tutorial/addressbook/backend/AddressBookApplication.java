dfd294b0a0476b02e9082c216356f25946c21d73
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-19 17:49:03 -0500

    added release branch

--- src/main/java/com/vaadin/tutorial/addressbook/backend/AddressBookApplication.java
+++ src/main/java/com/vaadin/tutorial/addressbook/backend/AddressBookApplication.java
@@ -2,10 +2,19 @@ package com.vaadin.tutorial.addressbook.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AddressBookApplication {
+public class AddressBookApplication extends SpringBootServletInitializer {
+
     public static void main(String[] args) {
         SpringApplication.run(AddressBookApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AddressBookApplication.class);
+    }
 }
+
