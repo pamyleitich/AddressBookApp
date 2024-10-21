b76378509e2d8e0cef3f195552ad96422d379549
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-21 16:54:57 -0500

    mongodb integration

--- src/main/java/com/addressbook/backend/controller/ContactController.java
+++ src/main/java/com/addressbook/backend/controller/ContactController.java
@@ -3,6 +3,8 @@ package com.addressbook.backend.controller;
 import com.addressbook.backend.model.Contact;
 import com.addressbook.backend.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,37 +18,72 @@ public class ContactController {
     private ContactRepository contactRepository;
 
     @GetMapping
-    public List<Contact> getAllContacts() {
-        return contactRepository.findAll();
+    public ResponseEntity<List<Contact>> getAllContacts() {
+        try {
+            List<Contact> contacts = contactRepository.findAll();
+            return new ResponseEntity<>(contacts, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Contact> getContactById(@PathVariable String id) {
+        try {
+            Optional<Contact> contact = contactRepository.findById(id);
+            if (contact.isPresent()) {
+                return new ResponseEntity<>(contact.get(), HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PostMapping
-    public Contact addContact(@RequestBody Contact contact) {
-        return contactRepository.save(contact);
+    public ResponseEntity<Contact> addContact(@RequestBody Contact contact) {
+        try {
+            Contact newContact = contactRepository.save(contact);
+            return new ResponseEntity<>(newContact, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @PutMapping("/{id}")
-    public Contact updateContact(@PathVariable String id, @RequestBody Contact contactDetails) {
-        Optional<Contact> optionalContact = contactRepository.findById(id);
-        
-        if (optionalContact.isPresent()) {
-            Contact contact = optionalContact.get();
-            contact.setFirstName(contactDetails.getFirstName());
-            contact.setLastName(contactDetails.getLastName());
-            contact.setEmail(contactDetails.getEmail());
-            contact.setDob(contactDetails.getDob());
-            contact.setPhone(contactDetails.getPhone());
-            return contactRepository.save(contact); 
-        } else {
-            throw new RuntimeException("Contact not found with id " + id);
+    public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contactDetails) {
+        try {
+            Optional<Contact> existingContact = contactRepository.findById(id);
+            if (existingContact.isPresent()) {
+                Contact contact = existingContact.get();
+                contact.setFirstName(contactDetails.getFirstName());
+                contact.setLastName(contactDetails.getLastName());
+                contact.setEmail(contactDetails.getEmail());
+                contact.setDob(contactDetails.getDob());
+                contact.setPhone(contactDetails.getPhone());
+
+                Contact updatedContact = contactRepository.save(contact);
+                return new ResponseEntity<>(updatedContact, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @DeleteMapping("/{id}")
-    public void deleteContact(@PathVariable String id) {
-        contactRepository.deleteById(id);
+    public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id) {
+        try {
+            contactRepository.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
+
 
 
 
