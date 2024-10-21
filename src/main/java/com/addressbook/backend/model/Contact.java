87fc65e202080c7e1acd6f1cc931536361cc3f02
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-20 21:47:03 -0500

    changes for proper app access

--- src/main/java/com/addressbook/backend/model/Contact.java
+++ src/main/java/com/addressbook/backend/model/Contact.java
@@ -5,15 +5,19 @@ public class Contact {
     private String firstName;
     private String lastName;
     private String email;
+    private String dob; // Date of Birth
+    private String phone; // Phone Number
 
     // Constructors, getters, setters
     public Contact() {}
 
-    public Contact(Long id, String firstName, String lastName, String email) {
+    public Contact(Long id, String firstName, String lastName, String email, String dob, String phone) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+        this.dob = dob;
+        this.phone = phone;
     }
 
     public Long getId() {
@@ -47,4 +51,21 @@ public class Contact {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
+
