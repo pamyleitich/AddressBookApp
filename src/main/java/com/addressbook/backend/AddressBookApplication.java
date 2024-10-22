3e8d9aba17610b6f6a10ab9cef6612b2a3179976
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-22 18:50:13 -0500

    mongodb integration

--- src/main/java/com/addressbook/backend/AddressBookApplication.java
+++ src/main/java/com/addressbook/backend/AddressBookApplication.java
@@ -2,11 +2,18 @@ package com.addressbook.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "com.addressbook.backend.repository") // Enable MongoDB repositories
-public class AddressBookApplication {
+@EnableMongoRepositories(basePackages = "com.addressbook.backend.repository")
+public class AddressBookApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AddressBookApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AddressBookApplication.class, args);
