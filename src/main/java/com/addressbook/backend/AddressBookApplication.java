dce4f4ccd65654d1334f67f2471673754c9884b6
Ndiforamang Fusi <fusisoft@gmail.com>
2024-11-06 14:43:42 -0600

    Adding enhanced addressbook code

--- src/main/java/com/addressbook/backend/AddressBookApplication.java
+++ src/main/java/com/addressbook/backend/AddressBookApplication.java
@@ -8,15 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.addressbook.backend.repository")
-@EnableSwagger2
 public class AddressBookApplication extends SpringBootServletInitializer {
 
     @Override
@@ -35,22 +29,13 @@ public class AddressBookApplication extends SpringBootServletInitializer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")  // Change origin as needed
+                        .allowedOrigins("http://localhost:3000")  // Adjust as needed
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
-
-    // Swagger Configuration for API Documentation
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.addressbook.backend.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
 }
+
 
