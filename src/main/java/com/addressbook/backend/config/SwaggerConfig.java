c8f6763a07342430067626386c5eda73bc91edb5
Ndiforamang Fusi <fusisoft@gmail.com>
2024-11-06 22:46:17 -0600

    Adding enhanced addressbook code

--- src/main/java/com/addressbook/backend/config/SwaggerConfig.java
+++ src/main/java/com/addressbook/backend/config/SwaggerConfig.java
@@ -2,11 +2,7 @@ package com.addressbook.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 @OpenAPIDefinition(
@@ -17,13 +13,7 @@ import springfox.documentation.spring.web.plugins.Docket;
     )
 )
 public class SwaggerConfig {
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.addressbook.backend.controller"))
-                .build();
-    }
+    // Configuration is now handled by springdoc-openapi annotations.
 }
+
 
